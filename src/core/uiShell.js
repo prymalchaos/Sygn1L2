eea9b2cbfd0ecbd48bba976667a931b5b1da3cdf
prymalchaos <114586585+prymalchaos@users.noreply.github.com>
2025-12-27 11:49:12 +1100

    Update uiShell.js

--- src/core/uiShell.js
+++ src/core/uiShell.js
@@ -1,1 +1,9 @@
-new
+export function createShell(root) {
+  root.innerHTML = `
+    <div id="shell">
+      <h1>SYGN1L2</h1>
+      <div id="plugin"></div>
+    </div>
+  `;
+  return root.querySelector("#plugin");
+}

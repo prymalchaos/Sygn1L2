944592a63697ec83ff3fdf003e6cf192cc35a0ba
prymalchaos <114586585+prymalchaos@users.noreply.github.com>
2025-12-27 11:46:54 +1100

    Update supabaseClient.js

--- src/core/supabaseClient.js
+++ src/core/supabaseClient.js
@@ -1,1 +1,13 @@
-new
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://raqqtppbtbcvkkenyqye.supabase.co",
+  "sb_publishable_T913yWlRXNOAJt_rp81R8Q_NLshMz_i",
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true
+    }
+  }
+);

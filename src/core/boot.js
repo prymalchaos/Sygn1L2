f2b4d988e65ef8131819b5cfec7b29fda97ef705
prymalchaos <114586585+prymalchaos@users.noreply.github.com>
2025-12-27 11:49:37 +1100

    Update boot.js

--- src/core/boot.js
+++ src/core/boot.js
@@ -1,1 +1,36 @@
-new
+import { createShell } from "./uiShell.js";
+import { loadPlugins, getPlugin } from "./pluginLoader.js";
+import { createDefaultState } from "./state.js";
+import { loadSave, saveState } from "./save.js";
+
+let state = createDefaultState();
+let activePlugin;
+
+async function start() {
+  await loadPlugins();
+
+  const saved = await loadSave();
+  if (saved) state = saved;
+
+  const app = document.getElementById("app");
+  const mount = createShell(app);
+
+  switchPhase(state.phase, mount);
+
+  setInterval(() => saveState(state), 30000);
+}
+
+async function switchPhase(id, mount) {
+  mount.innerHTML = "";
+  activePlugin = getPlugin(id);
+  activePlugin.mount(mount, {
+    getState: () => state,
+    setState: s => state = s,
+    setPhase: p => {
+      state.phase = p;
+      switchPhase(p, mount);
+    }
+  });
+}
+
+start();

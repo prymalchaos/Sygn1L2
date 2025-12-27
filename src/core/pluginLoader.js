7a2ebc0965947674ccc106e811247123db99eae0
prymalchaos <114586585+prymalchaos@users.noreply.github.com>
2025-12-27 11:48:29 +1100

    Update pluginLoader.js

--- src/core/pluginLoader.js
+++ src/core/pluginLoader.js
@@ -1,1 +1,13 @@
-nee
+const plugins = new Map();
+
+export async function loadPlugins() {
+  const phase0 = await import("../plugins/phase0_onboarding/plugin.js");
+  const phase1 = await import("../plugins/phase1/plugin.js");
+
+  plugins.set(phase0.default.id, phase0.default);
+  plugins.set(phase1.default.id, phase1.default);
+}
+
+export function getPlugin(id) {
+  return plugins.get(id);
+}

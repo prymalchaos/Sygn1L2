073870172c6bc8d9b7e3809c3fa07716a19bdaba
prymalchaos <114586585+prymalchaos@users.noreply.github.com>
2025-12-29 07:27:39 +1100

    Add files via upload

--- src/core/pluginLoader.js
+++ src/core/pluginLoader.js
@@ -4,7 +4,7 @@
 // Bump BUILD to force fresh plugin loads.
 
 const plugins = new Map();
-const BUILD = "2025-12-29b";
+const BUILD = "2025-12-29c";
 
 export async function loadPlugins() {
   const pluginIds = [
